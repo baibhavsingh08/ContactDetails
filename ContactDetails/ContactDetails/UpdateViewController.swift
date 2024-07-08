39c6d649bb7b322f2fea6e14141f466acd4d4a06
baibhavsingh08 <baibhav.singh@zopsmart.com>
2024-07-08 14:38:48 +0530

    final commit

--- ContactDetails/ContactDetails/UpdateViewController.swift
+++ ContactDetails/ContactDetails/UpdateViewController.swift
@@ -1,10 +1,3 @@
-//
-//  UpdateViewController.swift
-//  ContactDetails
-//
-//  Created by Raramuri on 05/07/24.
-//
-
 import UIKit
 
 protocol ContactDetailProtocol: AnyObject{
@@ -12,46 +5,20 @@ protocol ContactDetailProtocol: AnyObject{
 }
 
 class UpdateViewController: UIViewController{
-    
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var phoneTextField: UITextField!
     
     weak var delegate: ContactDetailProtocol?
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-
-
     }
-    
-    
-    
-    
-    
-    
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-    
     @IBAction func close(){
         let emailTextFieldValue = emailTextField.text ?? "no email provided"
         let phoneTextFieldValue = phoneTextField.text ?? "no phone number provided"
         
-        print("email is \(emailTextFieldValue)")
-        print("phone number is \(phoneTextFieldValue)")
-        
         delegate?.didUpdateDetails(email: emailTextFieldValue, phoneNumber: phoneTextFieldValue)
         navigationController?.popViewController(animated: true)
     }
-
 }
--- ContactDetails/ContactDetails/ViewController.swift
+++ ContactDetails/ContactDetails/ViewController.swift
@@ -1,10 +1,3 @@
-//
-//  ViewController.swift
-//  ContactDetails
-//
-//  Created by Raramuri on 05/07/24.
-//
-
 import UIKit
 
 extension ViewController: ContactDetailProtocol{
@@ -12,46 +5,27 @@ extension ViewController: ContactDetailProtocol{
         emailLabel.text = email
         phoneLabel.text = phoneNumber
     }
-    
-    
 }
 
-
-
 class ViewController: UIViewController {
-    
     @IBOutlet weak var image: UIImageView!
     @IBOutlet weak var emailLabel: UILabel!
     @IBOutlet weak var phoneLabel: UILabel!
     
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-    
-    //    override func viewDidLayoutSubviews(){
-    //        super.viewDidLayoutSubviews()
-    //        image.backgroundColor = .black
-    //        image.layer.cornerRadius = image.frame.size.width / 2
-    //        image.clipsToBounds = true
-    //    }
     
     override func viewDidLayoutSubviews(){
         super.viewDidLayoutSubviews()
         image.backgroundColor = .white
         image.layer.cornerRadius = image.frame.size.height/2
         image.clipsToBounds = true
-        
     }
     
     @IBAction func saveDetails(_ sender: UIButton){
-        let vc = self.storyboard?.instantiateViewController(withIdentifier : "second") as? UpdateViewController
+        let vc = self.storyboard?.instantiateViewController(withIdentifier: "second") as? UpdateViewController
         vc?.delegate = self
-        
         self.navigationController?.pushViewController(vc!, animated: true)
-        
-        
     }
 }
