7836cf0af7e20c2553f967a45ed0deb41a558c95
baibhavsingh08 <baibhav.singh@zopsmart.com>
2024-07-11 22:35:51 +0530

    use-default

--- ContactDetails/ContactDetails/UpdateViewController.swift
+++ ContactDetails/ContactDetails/UpdateViewController.swift
@@ -1,14 +1,27 @@
 import UIKit
 
-protocol ContactDetailProtocol: AnyObject{
-    func didUpdateDetails(email: String, phoneNumber: String)
+//protocol ContactDetailProtocol: AnyObject{
+//    func didUpdateDetails(email: String, phoneNumber: String)
+//}
+
+
+struct Keys{
+    static let email = "emailKey"
+    static let phone = "phoneKey"
 }
 
+let defaults = UserDefaults.standard
+
+
 class UpdateViewController: UIViewController{
+    
+    
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var phoneTextField: UITextField!
     
-    weak var delegate: ContactDetailProtocol?
+//    weak var delegate: ContactDetailProtocol?
+    
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,7 +31,15 @@ class UpdateViewController: UIViewController{
         let emailTextFieldValue = emailTextField.text ?? "no email provided"
         let phoneTextFieldValue = phoneTextField.text ?? "no phone number provided"
         
-        delegate?.didUpdateDetails(email: emailTextFieldValue, phoneNumber: phoneTextFieldValue)
+        if emailTextFieldValue != "" {
+            defaults.set(emailTextFieldValue, forKey: Keys.email)
+        }
+        
+        if phoneTextFieldValue != "" {
+            defaults.set(phoneTextFieldValue, forKey: Keys.phone)
+        }
+        
+//        delegate?.didUpdateDetails(email: emailTextFieldValue, phoneNumber: phoneTextFieldValue)
         navigationController?.popViewController(animated: true)
     }
 }
--- ContactDetails/ContactDetails/ViewController.swift
+++ ContactDetails/ContactDetails/ViewController.swift
@@ -1,19 +1,37 @@
 import UIKit
 
-extension ViewController: ContactDetailProtocol{
-    func didUpdateDetails(email: String, phoneNumber: String) {
-        emailLabel.text = email
-        phoneLabel.text = phoneNumber
-    }
-}
+
+
+//extension ViewController: ContactDetailProtocol{
+//    func didUpdateDetails(email: String, phoneNumber: String) {
+//        emailLabel.text = email
+//        phoneLabel.text = phoneNumber
+//    }
+//}
 
 class ViewController: UIViewController {
     @IBOutlet weak var image: UIImageView!
     @IBOutlet weak var emailLabel: UILabel!
     @IBOutlet weak var phoneLabel: UILabel!
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        let emailExist = defaults.string(forKey: Keys.email)
+        if(emailExist != nil) {
+            emailLabel.text = emailExist
+        }
+        
+        let phoneExist = defaults.string(forKey: Keys.phone)
+        if(phoneExist != nil) {
+            phoneLabel.text = phoneExist
+        }
     }
     
     override func viewDidLayoutSubviews(){
@@ -25,7 +43,7 @@ class ViewController: UIViewController {
     
     @IBAction func saveDetails(_ sender: UIButton){
         let vc = self.storyboard?.instantiateViewController(withIdentifier: "second") as? UpdateViewController
-        vc?.delegate = self
+//        vc?.delegate = self
         self.navigationController?.pushViewController(vc!, animated: true)
     }
 }
